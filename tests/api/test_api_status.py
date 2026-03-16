a456dd1881bcf651600f770b6122fa5bb40bbb46
Benjamin <wise9mind.solutions@gmail.com>
2026-03-16 11:48:18 -0400

    Add test markers

--- tests/api/test_api_status.py
+++ tests/api/test_api_status.py
@@ -1,6 +1,9 @@
+import pytest
 import requests
 
 
+@pytest.mark.api
+@pytest.mark.regression
 def test_api_status():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     assert response.status_code == 200
--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,8 +1,11 @@
+import pytest
 from pages.home_page import HomePage
 from utils.config import BASE_URL
 from utils.logger import logger
 
 
+@pytest.mark.smoke
+@pytest.mark.regression
 def test_homepage(page):
     logger.info("Starting homepage test")
     home_page = HomePage(page)
